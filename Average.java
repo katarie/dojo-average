6c4510339980d78799ad7209d59edaf7335171ca
valentin.veler2@gmail.com <Veler1996>
2017-09-29 11:56:47 +0200

    commit

--- Average.java
+++ Average.java
@@ -2,13 +2,15 @@ class Average {
 	
 	public static float average(int[] values) {
 		float total = 0;
-
+		
 		// début de ton code ici !
-
-
-
+		
+		for (int i = 0; i < values.length; i++) {
+			total = values[i] + total;
+		}
+		
 		// fin de ton code :)
-
+		
 		return total / values.length;
 	}
 }
